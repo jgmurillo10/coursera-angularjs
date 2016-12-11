6530c287657a44d5a45ed9b210be826751850dbb
Juan Guillermo Murillo Castillo <jg.murillo10@uniandes.edu.co>
2016-12-10 19:20:07 -0500

    06 donde

--- Lecture04/app.js
+++ Lecture04/app.js
@@ -1,11 +1,15 @@
 ( function (){
 'use strict';
 
-angular.module('myFirstApp', [])
+ angular.module('myFirstApp', [])
 
 
-.controller('MyFirstController', function(){
+.controller('MyFirstController', function($scope){
 
+	$scope.name="juan";
+	$scope.sayHello = function(){
+		return 'hello coursera'
+	};	
 });
 
 
