82fb3d423edb839d3f4dd4da9913ce8f7533cfa9
Juan Guillermo Murillo Castillo <jg.murillo10@uniandes.edu.co>
2016-12-20 22:54:15 -0500

    lecture 14 p.1

---
+++ Week 2/Lecture 14/app.js
@@ -0,0 +1,36 @@
+(function () {
+'use strict';
+
+angular.module('CounterApp', [])
+.controller('CounterController', CounterController);
+
+CounterController.$inject = ['$scope'];
+function CounterController($scope) {
+
+
+  $scope.onceCounter=0;
+  $scope.counter=0;
+  $scope.showNumberOfWatchers = function () {
+  	console.log( '# of Watchers: ', $scope.$$watchersCount);
+  };
+  $scope.countOnce = function () {
+  	$scope.onceCounter=1;
+  };
+  $scope.upCounter= function () {
+  	$scope.counter=$scope.counter+1;
+  }
+
+  $scope.$watch(function () {
+  	console.log('Digest Loop Fired!!')
+  })
+  // $scope.$watch('counter', function (newValue, oldValue) {
+  // 	console.log('Old value: ', oldValue );
+  // 	console.log('New value: ', newValue);
+  // })
+  //  $scope.$watch('onceCounter', function (newValue, oldValue) {
+  // 	console.log('Once counter Old value: ', oldValue );
+  // 	console.log('Once counter New value: ', newValue);
+  // })
+}
+
+})();
